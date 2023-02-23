2064907a28a19dbc743db29dbc52b220c1eee542
md masud rana <mr7123619@gmail.com>
2023-02-23 23:11:26 +0600

    remove consol

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8tifwil.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
